76a66a061a8fe9d1e3af935b2bbb5f498e601fce
Marco <mvl1729@gmailcom>
2019-02-18 15:11:05 +0100

    remove "readconfig function" which was based on the "set commands format" of the edgerouter configuration. The human readable format is easier to read and retrieve from the router, so this function is no longer required.
    Also added code to handle situation where there was not prior firewall_rules.csv

--- exportrules.py
+++ exportrules.py
@@ -54,51 +54,6 @@ def read_config_boot(file_path, rules):
     return True
 
 
-def readconfig(file_path, rules):
-    lookfor: str = 'rule'       # can be rule or description
-    searchpattern = ''          # pattern to find the description for the rule we just found
-    rule_elements = ''          # elements of the regular expression
-    lines = 0                   # how many lines have we tried to find a matching description
-    out_description = ''        # for output file: description
-    out_rule = ''               # for output file: rule
-
-    try:
-        f = open(file_path)
-    except FileNotFoundError:
-        print('Could not open file!')
-        return False
-    for line in f:
-        line = line.strip()
-        if lookfor == 'rule':
-            rule_elements = re.findall('(^set firewall name )(.+)( rule )(.+) action (.+)', line)
-            if rule_elements.__len__() > 0:
-                '-- build the string for this rule'
-                out_rule = rule_elements[0][1].strip() + '-' + rule_elements[0][3].strip() + '-' \
-                           + rule_elements[0][4][0].upper()
-                searchpattern = rule_elements[0][0] + rule_elements[0][1] + rule_elements[0][2] + \
-                                rule_elements[0][3] + ' ' + 'description (.+)'
-                lookfor = 'description'
-                lines = 0
-                out_description = ''
-        else:
-            # now search for the description - this may appear in the next line, or a bit further down ...
-            # if not found after 4 lines, we'll assume there is no description and continue searching for the next rule
-            rule_description = re.findall(searchpattern, line)
-            if rule_description.__len__() > 0:
-                out_description = rule_elements[0][1].strip() + '-' + rule_description[0]
-                out_description = out_description.replace("'", "")
-            else:
-                # not yet found, search in next line
-                lines += 1
-                if lines > 2:
-                    out_description = '-'
-            if out_description.__len__() > 0:
-                rules[out_rule] = out_rule + "," + out_description
-                lookfor = 'rule'
-    f.close()
-    return True
-
-
 def getfilename():
     root = tk.Tk()
     root.withdraw()
@@ -110,13 +65,18 @@ def readrules(directory, rules):
     # read existing rules, to be updated in the next step
     # this allows old rules (no longer relevant in the current configuration) to still be
     # available for matching in Splunk)
-    f = open(directory + '/' + FIREWALL_RULES_CSV)
-    first = True
-    for line in f:
-        if not first:
-            elements = line.split(',')
-            rules[elements[0]] = line.strip()
-        first = False
+    try:
+        f = open(directory + '/' + FIREWALL_RULES_CSV)
+    except FileNotFoundError:
+        # there are no existing rules yet - nothing to do here
+        elements = ''
+    else:
+        first = True
+        for line in f:
+            if not first:
+                elements = line.split(',')
+                rules[elements[0]] = line.strip()
+            first = False
 
 
 def writerules(directory, rules):
